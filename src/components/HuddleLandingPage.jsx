3a0b2358f8243e04c3fac8332882d7837e03451d
Jakub Lyko <jlyko@future-processing.com>
2022-01-10 19:22:27 +0100

    Added svg as React components

--- src/components/HuddleLandingPage.jsx
+++ src/components/HuddleLandingPage.jsx
@@ -1,8 +1,13 @@
 import './HuddleLandingPage.scss'
+import BackgroundDesktop from './images/BackgroundDesktop'
+import BackgroundMobile from './images/BackgroundMobile'
+import Logo from './images/Logo'
+import IllustrationMockups from './images/IllustrationMockups'
 
 const HuddleLandingPage = () => {
   return (
     <div>
+      <IllustrationMockups></IllustrationMockups>
     </div>
   )
 }
