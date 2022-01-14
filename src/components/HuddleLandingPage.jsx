dca6c10b898c61a1b1fbe96a11bff3ac74cf43e0
Jakub Lyko <jlyko@future-processing.com>
2022-01-14 22:58:26 +0100

    Added svg file as the img tag

--- src/components/HuddleLandingPage.jsx
+++ src/components/HuddleLandingPage.jsx
@@ -1,22 +1,18 @@
 import './HuddleLandingPage.scss'
-import BackgroundDesktop from './images/BackgroundDesktop'
-import BackgroundMobile from './images/BackgroundMobile'
-import Logo from './images/Logo'
-import IllustrationMockups from './images/IllustrationMockups'
 import SocialsSection from './SocialsSection'
 import texts from './texts'
 
 const HuddleLandingPage = () => {
   return (
     <div className='container'>
-      <BackgroundMobile />
-      <Logo className='logo' />
-      <IllustrationMockups className='illustrations' />
+      {/* <BackgroundMobile /> */}
+      {/* <Logo className='logo' /> */}
+      {/* <IllustrationMockups className='illustrations' /> */}
       <div className='title'>{texts.title}</div>
       <div className='content'>{texts.content}</div>
       <div className='button'>{texts.buttonAction}</div>
       <div className='socials'></div>
-      <img src='./images/images/illustration-mockup.svg' />
+      <img src='illustration-mockups.svg' />
       <SocialsSection />
     </div>
   )
